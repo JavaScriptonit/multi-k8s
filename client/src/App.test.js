23866296059d1e7832e380b1c15ba673d89c7a0d
Andrey Shabunov <shabunovaa@yandex.ru>
2022-06-01 12:21:36 +0300

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {});
+
+
+// https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/learn/lecture/11437334#announcements
+// удалены строчки внутри тест-сета
